b923811b5028c6717ad28d018600b7dc7ac707f6
Daniel Ahmed <Daniel.Ahmed@outlook.com>
2014-03-25 14:48:14 +0000

    Added ImageButton to LowerLetterSelectionActivity

--- src/uk/ac/mdx/LowerLetterSelectionActivity.java
+++ src/uk/ac/mdx/LowerLetterSelectionActivity.java
@@ -2,6 +2,9 @@ package uk.ac.mdx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class LowerLetterSelectionActivity extends Activity {
 
@@ -9,6 +12,18 @@ public class LowerLetterSelectionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lower_letter_selection);
+		
+		// Letter A
+		ImageButton LetterAButton = (ImageButton)findViewById(R.id.button_capital_a);
+		LetterAButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				// Create Letter A Activity
+				
+			}
+		});
 	}
 
 }
